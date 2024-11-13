72ac7cf400bd2a12d272260276693711fd774ae8
Timileyin Adefare <timileyinadefare@yahoo.com>
2024-11-13 22:51:16 +0100

    variables likely to adjust

--- k8s-cluster/variables.tf
+++ k8s-cluster/variables.tf
@@ -1,3 +1,43 @@
+variable "vsphere_user" {
+  description = "vSphere username"
+  default     = "administrator@vsphere.local"  # Change this to your vSphere username
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+  default     = "your-password"  # Change this to your vSphere password
+}
+
+variable "vsphere_server" {
+  description = "vSphere server address"
+  default     = "vsphere.example.com"  # Change this to your vSphere server's address
+}
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+  default     = "Datacenter"  # Change this to your datacenter name
+}
+
+variable "vsphere_cluster" {
+  description = "vSphere cluster"
+  default     = "Cluster"  # Change this to your cluster name
+}
+
+variable "vsphere_datastore" {
+  description = "Datastore for VM storage"
+  default     = "datastore1"  # Change this to your datastore name
+}
+
+variable "vsphere_network" {
+  description = "vSphere network"
+  default     = "VM Network"  # Change this to your network name
+}
+
+variable "template_name" {
+  description = "VM Template Name"
+  default     = "ubuntu-22.04-template"  # Change this to your template name
+}
+
 variable "vm_count" {
   description = "Number of worker nodes"
   default     = 3
